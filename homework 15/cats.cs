1241477a19a8a0ff09ad577ca95c1d32b5c14af9
muskrichard <39930430+muskrichard@users.noreply.github.com>
2018-06-19 17:40:32 -0500

    Add files via upload

---
+++ homework 15/cats.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW15
+{
+    class cats
+    {
+        public string _cName { get; set; }
+        public int _jump { get; set; }
+        public int _energy { get; set; }
+        public string _color { get; set; }
+
+
+        public cats(String name, int jump, int energy, String color)
+        {
+            _cName = name;
+            _jump = jump;
+            _energy = energy;
+            _color = color;
+        }
+
+    }
+
+}
