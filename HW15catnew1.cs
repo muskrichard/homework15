ef71e03761c1d453a82758072e4970938480108b
muskrichard <39930430+muskrichard@users.noreply.github.com>
2018-06-20 08:53:26 -0500

    Add files via upload

---
+++ HW15catnew1.cs
@@ -0,0 +1,21 @@
+﻿namespace HW15
+{
+    class HW15catnew
+    {
+        public string _cName { get; set; }
+        public int _jump { get; set; }
+        public int _energy { get; set; }
+        public string _color { get; set; }
+
+
+        public HW15catnew(string name, int jump, int energy, string color)
+        {
+            _cName = name;
+            _jump = jump;
+            _energy = energy;
+            _color = color;
+        }
+
+    }
+
+}
